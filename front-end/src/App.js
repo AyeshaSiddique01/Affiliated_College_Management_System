645c9c1668f2a5fe6ad600e0b20a325f911591f5
AyeshaSiddique01 <89693571+AyeshaSiddique01@users.noreply.github.com>
2023-04-08 01:19:09 +0500

    App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,76 +1,67 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Link
-} from "react-router-dom";  //npm i react-router-dom 
-import './App.css';
-// import DuePaper from './Components/DuePaper/DuePaper';
-// import Home from './Components/Home/Home';
-// import Login from './Components/Login/Login';
-// import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';  //npm i react-router-dom 
+import DuePaper from './Components/DuePaper/DuePaper';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Navbar/Navbar';
 import Notifications from './Components/Notifications/Notifications';
-// import RequestReceived from './Components/RequestReceived/RequestReceived';
-// import ResultPending from './Components/ResultPending/ResultPending';
-// import Settings from './Components/Settings/Settings';
-// import SignupExaminerInfo from './Components/SignupExaminerInfo/SignupExaminerInfo';
-// import SignupPersonalInfo from './Components/SignupPersonalInfo/SignupPersonalInfo';
-// import UploadPaper from './Components/UploadPaper/UploadPaper';
-// import UploadResult from './Components/UploadResult/UploadResult';
-// import ExaminerExp from './Components/ExaminerExp';
-// import ExaminerQualification from './Components/ExaminerQualification';
-// import Profile from './Components/Profile';
-// import Sidebar from './Components/Sidebar';
+import RequestReceived from './Components/RequestReceived/RequestReceived';
+import ResultPending from './Components/ResultPending/ResultPending';
+import Settings from './Components/Settings/Settings';
+import SignupExaminerInfo from './Components/SignupExaminerInfo/SignupExaminerInfo';
+import SignupPersonalInfo from './Components/SignupPersonalInfo/SignupPersonalInfo';
+import UploadPaper from './Components/UploadPaper/UploadPaper';
+import UploadResult from './Components/UploadResult/UploadResult';
+import ExaminerExp from './Components/ExaminerExp';
+import ExaminerQualification from './Components/ExaminerQualification';
+import Profile from './Components/Profile';
 import 'boxicons'
 
 function App() {
   return (
-    <div className="App"> 
-    {/* <UploadPaper></UploadPaper> */}
-    {/* <Settings></Settings> */}
-    {/* <Home></Home> */}
-    {/* <DuePaper></DuePaper> */}
-    {/* <Navbar></Navbar> */}
-    {/* <RequestReceived></RequestReceived> */}
-    {/* <ResultPending></ResultPending> */}
-    <Notifications></Notifications>
-    {/* <Router>
-      <Navbar>
-         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/SignupPersonalInfo" element={<SignupPersonalInfo/>} />
-          <Route path="/SignupExaminerInfo" element={<SignupExaminerInfo/>} />
-          <Route path="/ExaminerQualification" element={<ExaminerQualification/>} />
-          <Route path="/ExaminerExp" element={<ExaminerExp/>} />
-          <Route path="/UploadPaper" element={<UploadPaper/>} />
-          <Route path="/RequestRecieved" element={<RequestRecieved/>} />
-          <Route path="/Notifications" element={<Notifications/>} />
-          <Route path="/Profile" element={<Profile/>} />
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/DuePaper" element={<Home/>} />
-          <Route path="/ResultPending" element={<Home/>} />
-          <Route path="/UploadResult" element={<UploadResult/>} />
-        </Routes> 
-      </Navbar>
-    </Router>       */}
-     {/* <Router>
-      <Navbar>
-         <Routes>
-          <Route path="/Home" component={Home} />
-          <Route path="/Notifications" component={Notifications} />
-          <Route path="/DuePaper" component={DuePaper} />
-          <Route path="/ResultPending" component={ResultPending} />
-          <Route path="/Profile" component={Profile} />
+    <div className="App">
+      <Router>
+        <Navbar />
+        {/* <Switch> */}
+          <Route path="/" component={Login} />
           <Route path="/SignupPersonalInfo" component={SignupPersonalInfo} />
-          <Route path="/Settings" component={Settings} />
           <Route path="/SignupExaminerInfo" component={SignupExaminerInfo} />
-          <Route path="/Login" component={Login} />
-        </Routes> 
-      </Navbar>
-    </Router> */}
+          <Route path="/ExaminerQualification" component={ExaminerQualification} />
+          <Route path="/ExaminerExp" component={ExaminerExp} />
+          <Route path="/UploadPaper" component={UploadPaper} />
+          <Route path="/RequestRecieved" component={RequestReceived} />
+          <Route path="/Notifications" component={Notifications} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/Home" component={Home} />
+          <Route path="/DuePaper" component={Home} />
+          <Route path="/ResultPending" component={Home} />
+          <Route path="/UploadResult" component={UploadResult} />
+        {/* </Switch> */}
+        {/* </Navbar> */}
+      </Router >
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <Router>
+//          <Navbar />
+//            <Route exact path="/" component={Login} />
+//            <Route path="/SignupPersonalInfo" component={SignupPersonalInfo} />
+//            <Route path="/SignupExaminerInfo" component={SignupExaminerInfo} />
+//            <Route path="/ExaminerQualification" component={ExaminerQualification} />
+//            <Route path="/ExaminerExp" component={ExaminerExp} />
+//            <Route path="/UploadPaper" component={UploadPaper} />
+//            <Route path="/RequestRecieved" component={RequestReceived} />
+//            <Route path="/Notifications" component={Notifications} />
+//            <Route path="/Profile" component={Profile} />
+//            <Route path="/Home" component={Home} />
+//            <Route path="/DuePaper" component={Home} />
+//            <Route path="/ResultPending" component={Home} />
+//            <Route path="/UploadResult" component={UploadResult} />
+//        </Router >
+//   );
+// }
 
 export default App
