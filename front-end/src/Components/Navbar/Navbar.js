6ecc5c551c3ea0469182744cf1a4ab8c059f5ebd
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-04-08 15:51:56 -0700

    better than previous:)

--- front-end/src/Components/Navbar/Navbar.js
+++ front-end/src/Components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const routes = [
 ]
 const Navbar = () => {
   return (
-    <div className='Navbar row'>
+    <div className='Navbar'>
       <div className="col-3">
       <a href="/" className='nav-logo'><box-icon type='logo' name='edge' animation='tada' color='#DDDBCB' size='45px'></box-icon>xaminer Portal</a>
       </div>
@@ -44,10 +44,6 @@ const Navbar = () => {
              <button  className="btnsubmit" type="submit"><box-icon name='search-alt' animation='tada' color='#DDDBCB' size='45px'></box-icon></button>
           </div>
         </form>
-        {/* <a href="/Home">Home</a>
-        <a href="/Notifications">Requests</a>
-        <a href="/DuePaper">Due Paper</a>
-        <a href="ResultPending">Result Pending</a> */}
       </div>
       <div className='header-btn col-3' style={{"text-align" : "end"}}>
         <a href="/Profile" className='progap'><box-icon name='user-circle' size='30px' color="#DDDBCB"></box-icon></a>
@@ -56,7 +52,6 @@ const Navbar = () => {
       </div>
       <div className='header-btn col-2'  style={{"text-align" : "end"}}>
         <a href="/SignupPersonalInfo" className='signlog'><box-icon name='log-out' size='22px' color="#DDDBCB"></box-icon>Log Out</a>
-        {/* <a href="/Login" className='signlog'>Log In</a> */}
       </div>
       <div className='VerNavbar'>
         <div className='vernav'>
@@ -66,14 +61,6 @@ const Navbar = () => {
           <a href="ResultPending">Result Pending</a>
         </div>
       </div>
-      {/* <div className='VerNavbar'>
-      <div className='vernav'>
-        <NavLink exact to="/" activeClassName="active">Home</NavLink>            
-        <NavLink to="/Notifications" activeClassName="active">Requests</NavLink>    
-        <NavLink to="/DuePaper" activeClassName="active">Due Paper</NavLink>         
-        <NavLink to="/ResultPending" activeClassName="active">Result Pending</NavLink> 
-      </div>
-    </div> */}
     </div>
     
 
